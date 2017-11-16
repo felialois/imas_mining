811df73124c72056d929bc1d66d35a5a99005f82
Santos Bringas Tejero <santi_bt@hotmail.com>
2017-11-16 13:07:16 +0100

    UPDATE
    UPDATE GenerateGameSettings.java

--- src/cat/urv/imas/onthology/GenerateGameSettings.java
+++ src/cat/urv/imas/onthology/GenerateGameSettings.java
@@ -6,6 +6,8 @@
 package cat.urv.imas.onthology;
 
 import static cat.urv.imas.onthology.InitialGameSettings.*;
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Random;
@@ -15,6 +17,13 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * Helper for updating the game settings. To do so, just update the content of
@@ -35,47 +44,83 @@ public class GenerateGameSettings {
      */
     public static void defineSettings(InitialGameSettings settings) {
         //add here whatever settings.set* to define your new settings.
-        settings.setSeed(1234567890);
-        settings.setNewMetalProbability(40);
-        settings.setMaxNumberFieldsWithNewMetal(3);
-        settings.setMaxAmountOfNewMetal(6);
-        settings.setDiggersCapacity(6);
-        settings.setManufacturingCenterPrice(new int[]{8, 9, 6, 7});
-        settings.setManufacturingCenterMetalType(new MetalType[]{
-            MetalType.GOLD,
-            MetalType.SILVER,
-            MetalType.SILVER,
-            MetalType.GOLD
-        });
-        settings.setSimulationSteps(STEPS);
-        settings.setTitle("Practical IMAS");
-        settings.setNumberInitialElements(10);
-        settings.setNumberVisibleInitialElements(10);
-        // settings for first date
-        int[][] map
-            = {
-                {F, F, F, MCC, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F},
-                {F, P, P, P, P, P, P, P, P, P, P, DC, P, P, P, P, P, P, P, F},
-                {F, P, PC, P, P, P, P, DC, P, P, P, P, P, P, P, P, P, P, DC, F},
-                {F, P, P, F, F, F, F, F, F, P, P, F, F, F, F, F, F, F, F, F},
-                {F, P, P, F, F, F, F, F, MCC, P, P, F, F, F, F, F, F, F, F, F},
-                {F, PC, P, F, F, P, P, P, P, P, P, F, F, P, P, P, P, P, P, F},
-                {F, P, P, F, F, P, P, P, P, P, P, F, F, P, P, P, P, P, P, F},
-                {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
-                {F, P, P, F, F, P, DC, F, F, P, P, F, F, P, P, F, F, P, P, F},
-                {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
-                {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
-                {F, P, P, F, F, P, P, F, F, P, PC, F, F, P, P, F, F, P, P, F},
-                {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, MCC, F, P, P, F},
-                {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, DC, P, F},
-                {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, DC, P, F},
-                {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
-                {F, P, P, MCC, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
-                {F, P, PC, P, DC, P, P, F, F, P, P, P, P, P, P, F, F, P, P, F},
-                {F, P, P, P, P, P, P, F, F, P, P, P, P, P, P, F, F, DC, P, F},
-                {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F},
-            };
-        settings.setInitialMap(map);
+        try {
+            // We start reading the file
+            File file = new File("userdata.xml");
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document doc = documentBuilder.parse(FILENAME);
+            doc.getDocumentElement().normalize();
+            // Now we can read the tags
+            String seed = doc.getElementsByTagName("seed").item(0).getTextContent();
+            settings.setSeed(Integer.parseInt(seed));
+            String newMetalProb = doc.getElementsByTagName("newMetalProbability").item(0).getTextContent();
+            settings.setNewMetalProbability(Integer.parseInt(newMetalProb));
+            String maxNumberFields = doc.getElementsByTagName("maxNumberFieldsWithNewMetal").item(0).getTextContent();
+            settings.setMaxNumberFieldsWithNewMetal(Integer.parseInt(maxNumberFields));
+            String maxAmountMetal = doc.getElementsByTagName("maxAmountOfNewMetal").item(0).getTextContent();
+            settings.setMaxAmountOfNewMetal(Integer.parseInt(maxAmountMetal));
+            String diggersCapacity = doc.getElementsByTagName("diggersCapacity").item(0).getTextContent();
+            settings.setDiggersCapacity(Integer.parseInt(diggersCapacity));
+            
+            NodeList prices = doc.getElementsByTagName("manufacturingCenterPrice");
+            int[] manufacturingPrices = new int[prices.getLength()];
+            for(int i = 0; i < prices.getLength(); i++) {
+                manufacturingPrices[i] = Integer.parseInt(prices.item(i).getTextContent());
+            }
+            settings.setManufacturingCenterPrice(manufacturingPrices);
+            
+            NodeList metalTypes = doc.getElementsByTagName("manufacturingCenterMetalType");
+            MetalType[] manufacturingMetalTypes = new MetalType[metalTypes.getLength()];
+            for(int i = 0; i < metalTypes.getLength(); i++) {
+                String metal = metalTypes.item(i).getTextContent();
+                if(metal.equals("GOLD"))
+                    manufacturingMetalTypes[i] = MetalType.GOLD;
+                else
+                    manufacturingMetalTypes[i] = MetalType.SILVER;
+            }
+            settings.setManufacturingCenterMetalType(manufacturingMetalTypes);
+            
+            String simulationSteps = doc.getElementsByTagName("simulationSteps").item(0).getTextContent();
+            settings.setSimulationSteps(Integer.parseInt(simulationSteps));
+            String title = doc.getElementsByTagName("title").item(0).getTextContent();
+            settings.setTitle(title);
+            String initialElements = doc.getElementsByTagName("numberInitialElements").item(0).getTextContent();
+            settings.setNumberInitialElements(Integer.parseInt(initialElements));
+            String initialVisibleElements = doc.getElementsByTagName("numberVisibleInitialElements").item(0).getTextContent();
+            settings.setNumberVisibleInitialElements(Integer.parseInt(initialVisibleElements));
+            
+            // Now we get the map
+            NodeList mapList = doc.getElementsByTagName("initialMap");
+            System.out.println(mapList.item(0));
+            int[][] map = new int[mapList.getLength()][3];
+            // settings for first date
+            int[][] firstMap
+                = {
+                    {F, F, F, MCC, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F},
+                    {F, P, P, P, P, P, P, P, P, P, P, DC, P, P, P, P, P, P, P, F},
+                    {F, P, PC, P, P, P, P, DC, P, P, P, P, P, P, P, P, P, P, DC, F},
+                    {F, P, P, F, F, F, F, F, F, P, P, F, F, F, F, F, F, F, F, F},
+                    {F, P, P, F, F, F, F, F, MCC, P, P, F, F, F, F, F, F, F, F, F},
+                    {F, PC, P, F, F, P, P, P, P, P, P, F, F, P, P, P, P, P, P, F},
+                    {F, P, P, F, F, P, P, P, P, P, P, F, F, P, P, P, P, P, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
+                    {F, P, P, F, F, P, DC, F, F, P, P, F, F, P, P, F, F, P, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, PC, F, F, P, P, F, F, P, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, MCC, F, P, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, DC, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, DC, P, F},
+                    {F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
+                    {F, P, P, MCC, F, P, P, F, F, P, P, F, F, P, P, F, F, P, P, F},
+                    {F, P, PC, P, DC, P, P, F, F, P, P, P, P, P, P, F, F, P, P, F},
+                    {F, P, P, P, P, P, P, F, F, P, P, P, P, P, P, F, F, DC, P, F},
+                    {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F},
+                };
+            settings.setInitialMap(map);
+        } catch(IOException | ParserConfigurationException | DOMException | SAXException e) {
+        }
     }
 
     /*
