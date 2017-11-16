c8f4ab4f1d74041deebadf4479010d99cc474c9d
Felipe Alfaro <felialois@gmail.com>
2017-11-16 12:30:40 +0100

    First Commit

---
+++ src/cat/urv/imas/agent/DiggerAgent.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.urv.imas.agent;
+
+import static cat.urv.imas.agent.ImasAgent.OWNER;
+import cat.urv.imas.behaviour.coordinator.RequesterBehaviour;
+import cat.urv.imas.onthology.MessageContent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+
+/**
+ *
+ * @author felipe
+ */
+public class DiggerAgent extends WorkerAgent{
+    
+    public DiggerAgent() {
+        super(AgentType.DIGGER);
+    }
+    
+    @Override
+    public void setup(){
+        /* ** Very Important Line (VIL) ***************************************/
+        this.setEnabledO2ACommunication(true, 1);
+        /* ********************************************************************/
+
+        // Register the agent to the DF
+        ServiceDescription sd1 = new ServiceDescription();
+        sd1.setType(AgentType.DIGGER.toString());
+        sd1.setName(getLocalName());
+        sd1.setOwnership(OWNER);
+        
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.addServices(sd1);
+        dfd.setName(getAID());
+        try {
+            DFService.register(this, dfd);
+            log("Registered to the DF");
+        } catch (FIPAException e) {
+            System.err.println(getLocalName() + " registration with DF unsucceeded. Reason: " + e.getMessage());
+            doDelete();
+        }
+        // search for the correct coordinator
+        ServiceDescription searchCriterion = new ServiceDescription();
+        searchCriterion.setType(AgentType.DIGGER_COORDINATOR.toString());
+        this.coordinator = UtilsAgents.searchAgent(this, searchCriterion);
+        // searchAgent is a blocking method, so we will obtain always a correct AID
+
+    }
+    
+}
