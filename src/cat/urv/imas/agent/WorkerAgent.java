ecf5abf2132e31e4062c8d9dd3533a227e68ab64
marinakreplak <marina.kreplak@polymtl.ca>
2018-01-28 18:31:39 +0100

    WorkerAgent Location attributes

--- src/cat/urv/imas/agent/WorkerAgent.java
+++ src/cat/urv/imas/agent/WorkerAgent.java
@@ -7,6 +7,7 @@ package cat.urv.imas.agent;
 
 import cat.urv.imas.onthology.GameSettings;
 import jade.core.AID;
+import java.util.List;
 
 /**
  *
@@ -15,8 +16,8 @@ import jade.core.AID;
 public class WorkerAgent extends ImasAgent{
     
     protected AID coordinator;
-    
     private GameSettings game;
+    private int column, row;  
     
     public void setGame(GameSettings game) {
         this.game = game;
@@ -33,6 +34,24 @@ public class WorkerAgent extends ImasAgent{
     
     public WorkerAgent(AgentType type) {
         super(type);
+        this.column = this.game.getAgentList().get(this.type).get(Integer.valueOf(this.getName().substring(3))).getCol();
+        this.row = this.game.getAgentList().get(this.type).get(Integer.valueOf(this.getName().substring(3))).getRow();
     }
+    
+    public void moveRight(){
+        // TODO: Check if movement is valid (Path cell, Not occupied...)
+        this.column ++;
+        // TODO: Update map
+    }
+    
+    /*public void moveLeft(){
+        this.column --;
+    }
+    public void moveUp(){
+        this.row --;
+    }
+    public void moveDown(){
+        this.row ++;
+    }*/
     
 }
